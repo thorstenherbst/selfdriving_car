f66febdc290baff50bf6183dd6b61cde2f6155a2
therbst <herbst.thorsten@gmail.com>
2024-09-17 13:09:33 +0200

    delete unused apps

--- apps/tttgame/src/app/app.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Route } from '@angular/router';
-import { AppComponent } from './app.component';
-
-import('./remote-entry/entry.routes');
-export const appRoutes: Route[] = [
-  { path: '', redirectTo: 'start', pathMatch: 'full'},
-  { path: 'start', component: AppComponent},
-];
--- apps/tttgame/src/app/remote-entry/entry.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Route } from '@angular/router';
-import { RemoteEntryComponent } from './entry.component';
-import { appRoutes } from '../app.routes';
-
-export const remoteRoutes: Route[] = [
-  { path: '', component: RemoteEntryComponent,
-    children: appRoutes
-  },
-];
